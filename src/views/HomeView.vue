e48c4fce3947a81786249d2c3605122916c91887
pengchengyuan <pengchengyuan@hellogroup.com>
2023-07-15 23:56:45 +0800

    feat: init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import StoreList from "@/components/Store/StoreList.vue";
+import DietSodaEvents from "diet-soda-engine";
+
+console.log('DietSodaEvents', DietSodaEvents);
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <StoreList
+      :storeGoods="[
+        {
+          wood: {
+            key: 'wood',
+            name: 'Wood',
+            value: 0,
+            capacity: 100,
+          },
+        },
+      ]"
+    />
   </main>
 </template>
